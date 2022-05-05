9a4726b1fa2ad5588baaa9055afb3262052374ce
Luis Berrospi <luis.berrospi@utec.edu.pe>
2022-05-04 21:58:07 -0500

    owo

--- filter/kernel.cu
+++ filter/kernel.cu
@@ -10,7 +10,7 @@
 
 
 
-#define from2Dto1D(i, j) (((i) * gridDim.x) + (j))
+#define from2Dto1D(i, j) (((j) * gridDim.x) + (i))
 __global__ void betterCombKernel(const unsigned char* source, unsigned char* target, const int* kernel, int kernelDim, int divisor)
 {
 
@@ -25,7 +25,7 @@ __global__ void betterCombKernel(const unsigned char* source, unsigned char* tar
     for (int i = 0; i < kernelDim; ++i)
     {
         for (int j = 0; j < kernelDim; ++j) {
-            value += kernel[i * kernelDim + j] * source[from2Dto1D(x + (i - kernelDim / 2), y + (j - kernelDim / 2))];
+            value += kernel[j * kernelDim + i] * source[from2Dto1D(x + (i - kernelDim / 2), y + (j - kernelDim / 2))];
         }
     }
 
@@ -49,7 +49,7 @@ int main() {
 
     std::string folder = "C:/Users/luis/Desktop/source/";
 
-    cv::Mat img = cv::imread(folder + "input.jpg");
+    cv::Mat img = cv::imread(folder + "input.tif");
 
 
 
@@ -62,8 +62,9 @@ int main() {
     unsigned char* targetB;
     int* kernel;
     
-    const int DIM = img.size().height;
-    dim3 grid(DIM, DIM);
+    const int height = img.size().height;
+    const int width = img.size().width;
+    dim3 grid(width, height);
 
 
     /*int kernelH[] = { 1, 4, 6, 4, 1,
@@ -72,7 +73,13 @@ int main() {
                      4, 16, 24, 16, 4,
                      1, 4, 6, 4, 1};*/
 
-    /*int kernelH[] = {
+    /*int kernelH[] = { 1, 4, 6, 4, 1,
+                    4, 16, 24, 16, 4,
+                    2, 24, -476, 24, 2,
+                    4, 16, 24, 16, 4,
+                    1, 4, 6, 4, 1};*/
+
+    int kernelH[] = {
         0, 0, 1, 2, 1, 0, 0,
         0, 3, 13, 22, 13, 3, 0,
         1, 13, 59, 97, 59, 13, 1,
@@ -80,46 +87,48 @@ int main() {
         1, 13, 59, 97, 59, 13, 1,
         0, 3, 13, 22, 13, 3, 0,
         0, 0, 1, 2, 1, 0, 0,
+    };
 
-    };*/
 
+    
 
-    int kernelH[] = { -1, 0, 1,
+   /* int kernelH[] = { -1, 0, 1,
                       -1, 0, 1,
-                      -1, 0, 1 };
+                      -1, 0, 1 };*/
+    int divisor = 1003;
+
 
     int kernelDim = sqrt(sizeof(kernelH)/sizeof(int));
-    int divisor = 1;
 
     cudaMalloc((void**)&kernel, kernelDim * kernelDim * sizeof(int));
 
 
-    cudaMalloc((void**)&sourceR, DIM * DIM * sizeof(char));
-    cudaMalloc((void**)&sourceG, DIM * DIM * sizeof(char));
-    cudaMalloc((void**)&sourceB, DIM * DIM * sizeof(char));
-    cudaMalloc((void**)&targetR, (DIM) * (DIM) * sizeof(char));
-    cudaMalloc((void**)&targetG, (DIM) * (DIM) * sizeof(char));
-    cudaMalloc((void**)&targetB, (DIM) * (DIM) * sizeof(char));
+    cudaMalloc((void**)&sourceR, width * height * sizeof(char));
+    cudaMalloc((void**)&sourceG, width * height * sizeof(char));
+    cudaMalloc((void**)&sourceB, width * height * sizeof(char));
+    cudaMalloc((void**)&targetR, width * height * sizeof(char));
+    cudaMalloc((void**)&targetG, width * height * sizeof(char));
+    cudaMalloc((void**)&targetB, width * height * sizeof(char));
 
 
 
-    uchar* imgMatrixR = new uchar[DIM * DIM];
-    uchar* imgMatrixG = new uchar[DIM * DIM];
-    uchar* imgMatrixB = new uchar[DIM * DIM];
+    uchar* imgMatrixR = new uchar[width * height];
+    uchar* imgMatrixG = new uchar[width * height];
+    uchar* imgMatrixB = new uchar[width * height];
 
-    for (int col = 0; col < DIM; ++col) {
-        for (int row = 0; row < DIM; ++row) {
+    for (int col = 0; col < height; ++col) {
+        for (int row = 0; row < width; ++row) {
             auto v = img.at<cv::Vec3b>(col, row);
-            imgMatrixR[DIM * col + row] = v[0];
-            imgMatrixG[DIM * col + row] = v[1];
-            imgMatrixB[DIM * col + row] = v[2];
+            imgMatrixR[width * col + row] = v[0];
+            imgMatrixG[width * col + row] = v[1];
+            imgMatrixB[width * col + row] = v[2];
         }
     }
 
     cudaMemcpy(kernel, kernelH, kernelDim * kernelDim * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(sourceR, imgMatrixR, DIM * DIM * sizeof(char), cudaMemcpyHostToDevice);
-    cudaMemcpy(sourceG, imgMatrixG, DIM * DIM * sizeof(char), cudaMemcpyHostToDevice);
-    cudaMemcpy(sourceB, imgMatrixB, DIM * DIM * sizeof(char), cudaMemcpyHostToDevice);
+    cudaMemcpy(sourceR, imgMatrixR, width * height * sizeof(char), cudaMemcpyHostToDevice);
+    cudaMemcpy(sourceG, imgMatrixG, width * height * sizeof(char), cudaMemcpyHostToDevice);
+    cudaMemcpy(sourceB, imgMatrixB, width * height * sizeof(char), cudaMemcpyHostToDevice);
     
  
 
@@ -128,16 +137,16 @@ int main() {
     betterCombKernel << < grid, 1 >> > (sourceB, targetB, kernel, kernelDim, divisor);
 
 
-    cudaMemcpy(imgMatrixR, targetR, DIM * DIM * sizeof(char), cudaMemcpyDeviceToHost);
-    cudaMemcpy(imgMatrixG, targetG, DIM * DIM * sizeof(char), cudaMemcpyDeviceToHost);
-    cudaMemcpy(imgMatrixB, targetB, DIM * DIM * sizeof(char), cudaMemcpyDeviceToHost);
+    cudaMemcpy(imgMatrixR, targetR, width * height * sizeof(char), cudaMemcpyDeviceToHost);
+    cudaMemcpy(imgMatrixG, targetG, width * height * sizeof(char), cudaMemcpyDeviceToHost);
+    cudaMemcpy(imgMatrixB, targetB, width * height * sizeof(char), cudaMemcpyDeviceToHost);
 
-    for (int col = kernelDim/2; col < DIM; ++col) {
-        for (int row = kernelDim/2; row < DIM; ++row) {
+    for (int col = kernelDim/2; col < height; ++col) {
+        for (int row = kernelDim/2; row < width; ++row) {
             cv::Vec3b v = filterImgMat.at<cv::Vec3b>(col, row);
-            v[0] = imgMatrixR[DIM * row + col];
-            v[1] = imgMatrixG[DIM * row + col];
-            v[2] = imgMatrixB[DIM * row + col];
+            v[0] = imgMatrixR[width * col + row];
+            v[1] = imgMatrixG[width * col + row];
+            v[2] = imgMatrixB[width * col + row];
             filterImgMat.at<cv::Vec3b>(col, row) = v;
         }
     }
